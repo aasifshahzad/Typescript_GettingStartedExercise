7a5bebe99f320c3a7ddcb2cc41b74e107fe9aa3d
aasifshahzad <71362700+aasifshahzad@users.noreply.github.com>
2023-04-04 02:25:10 +0500

    1~27 solution

---
+++ task6.js
@@ -0,0 +1,13 @@
+//You should create four lines that look like this:
+//Your output should simply be four lines with the number 8 appearing once on each line.
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+import { calculator } from "../calculator.js";
+console.log("\n\n__________________________________________________\n\n");
+console.log(calculator(4, "+", 4));
+console.log("\n\n__________________________________________________\n\n");
+console.log(calculator(12, "-", 4));
+console.log("\n\n__________________________________________________\n\n");
+console.log(calculator(4, "*", 2));
+console.log("\n\n__________________________________________________\n\n");
+console.log(calculator(32, "/", 4));
+console.log("\n\n__________________________________________________\n\n");
---
+++ task9.js
@@ -0,0 +1,35 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner
+let guestList = ["asif", "saqib", "sadiq", "wasif"];
+let myaddress = "Camp Town, Florida";
+for (let index = 0; index < guestList.length; index++) {
+    let print = `Sir, ${guestList[index].toUpperCase()} you are invited to dinner on 23 August. Venue would be ${myaddress}.`;
+    console.log(print);
+}
+//Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+let guestAddition = ["Jamal", "kashif", "Nasir", "wasif"];
+let guestDeletion = ["sadiq", "asif", "shumail"];
+//console.log(guestList);
+guestList = guestList.concat(guestAddition); // Add the name of the guest to be added
+//console.log(guestList);
+guestList = Array.from(new Set(guestList)); // remove duplicate strings in an array of string 
+console.log(`Guest list after adding new guests and removing duplicates ${guestList}`);
+//console.log(guestList.find((element) => element === "asif")); // find and element in an array
+for (let index = 0; index < guestDeletion.length; index++) { // iterate through the guest Deletion list
+    let gName = guestList.find((element) => element === guestDeletion[index]); // check if the name of the guest to be deleted is in the main guest list    
+    if (gName) {
+        gName = gName.toString();
+        //console.log(gName);
+        let indexOfGname = guestList.indexOf(gName); // find index of the guest name
+        //console.log(indexOfGname);
+        console.log(`Guest name ${guestDeletion[index]} is in Guest List`); // display the name of the guest to be deleted is in guest list
+        let newGuestList = guestList.splice(indexOfGname, 1);
+        //console.log(newGuestList);
+        console.log(`The guest name ${guestDeletion[index]} deleted from Guest list`);
+    }
+    else {
+        console.log(`Guest name ${guestDeletion[index]} is not in Guest List`);
+    }
+}
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+console.log(`You have invided ${guestList.length} guests on dinner.`);
+export {};
